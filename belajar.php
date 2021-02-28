11ea92f6e93f52b159abac4ccb4958771e792518
Muzakky14 <a.muzakky14@gmail.com>
2021-02-28 22:49:08 +0700

    belajar php

---
+++ belajar.php
@@ -0,0 +1,23 @@
+<?php
+
+    echo"hello world!";
+    echo "<br>";
+
+    $nama = "Muzakky";
+    $angka = 19;
+    $angka_desimal = 20.3;
+
+    echo "nama saya adalah :".$nama;
+    echo "<br>";
+    echo "Umur saya :".$angka."tahun";
+    echo "<br>";
+
+    echo 'Hello world! Selamat hari "jumat"';
+
+    echo "<h1>hello world</h1>";
+
+    echo "dokumen :".$_SERVER["DOCUMENT_ROOT"];
+
+    echo "dokumen :".$_SERVER["PHP_SELF"];
+
+?>
